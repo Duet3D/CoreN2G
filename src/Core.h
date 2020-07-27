d6a6b726b6450c5b275b3e3730dcbf91b1ea573f
David Crocker <dcrocker@eschertech.com>
2020-07-27 13:59:02 +0100

    Added Doxygen comments to file Core.h

--- src/Core.h
+++ src/Core.h
@@ -1,11 +1,13 @@
-/*
- * Core.h
- *
- *  Created on: 19 Jun 2020
- *      Author: David
+/**
+ * @file Core.h
+ * @brief Header file declaring types, functions etc. usable from both C and C++
  *
  * This file contains basic CPU and I/O pin support for the SAME5x (also works with SAMD5x) and SAMC21
  * Use it where we can't include the full CoreIO.h file, for example in C source files
+ */
+
+/*  Created on: 19 Jun 2020
+ *      Author: David
  */
 
 #ifndef SRC_CORE_H_
@@ -43,19 +45,20 @@
 #include <ctype.h>
 
 // Types
-typedef uint8_t DmaChannel;
-typedef uint8_t DmaPriority;
-typedef uint8_t Pin;
-typedef uint8_t ExintNumber;
-typedef uint16_t PwmFrequency;		// type used to represent a PWM frequency. 0 sometimes means "default".
-typedef uint32_t NvicPriority;
 
-static const Pin NoPin = 0xFF;
+typedef uint8_t DmaChannel;			///< A type that represents a DMA channel number
+typedef uint8_t DmaPriority;		///< A type that represents a DMA priority
+typedef uint8_t Pin;				///< A type that represents an I/O pin on the microcontroller
+typedef uint8_t ExintNumber;		///< A type that represents an EXINT number. Used in pin tables.
+typedef uint16_t PwmFrequency;		///< A type that represents a PWM frequency. 0 sometimes means "default".
+typedef uint32_t NvicPriority;		///< A type that represents an interrupt priority
+
+static const Pin NoPin = 0xFF;		///< A number that represents no I/O pin
 
 // Standard GCLK numbers and frequencies
 #if SAME5x
 
-static const uint32_t SystemCoreClockFreq = 120000000;
+static const uint32_t SystemCoreClockFreq = 120000000;		///< The processor clock frequency after initialisation
 
 static const unsigned int GclkNum120MHz = 0;
 static const unsigned int GclkNum32KHz = 1;		// frequency is approx. 32768Hz
@@ -74,30 +77,33 @@ static const unsigned int GclkNum32KHz = 3;		// frequency is approx. 32768Hz
 
 #endif
 
-// Pin mode enumeration. Would ideally be a C++ scoped enum, but we need to use it from C library functions.
+/// Pin mode enumeration
 enum PinMode
 {
-	PIN_MODE_NOT_CONFIGURED = -1,	// used in Platform class to record that the mode for a pin has not been set yet
-	INPUT = 0,						// pin is a digital input
-	INPUT_PULLUP,					// pin is a digital input with pullup enabled
-	INPUT_PULLDOWN,					// pin is a digital input with pulldown enabled
-	OUTPUT_LOW,						// pin is an output with initial state LOW
-	OUTPUT_HIGH,					// pin is an output with initial state HIGH
-	AIN,							// pin is an analog input, digital input buffer is disabled if possible
-	SPECIAL,						// pin is used for the special function defined for it in the variant.cpp file
-	OUTPUT_PWM_LOW,					// PWM output mode, initially low
-	OUTPUT_PWM_HIGH,				// PWM output mode, initially high
+	PIN_MODE_NOT_CONFIGURED = -1,	///< do not use
+	INPUT = 0,						///< pin is a digital input
+	INPUT_PULLUP,					///< pin is a digital input with pullup enabled
+	INPUT_PULLDOWN,					///< pin is a digital input with pulldown enabled
+	OUTPUT_LOW,						///< pin is an output with initial state LOW
+	OUTPUT_HIGH,					///< pin is an output with initial state HIGH
+	AIN,							///< pin is an analog input, digital input buffer is disabled if possible
+	SPECIAL,						///< do not use
+	OUTPUT_PWM_LOW,					///< PWM output mode, initially low
+	OUTPUT_PWM_HIGH,				///< PWM output mode, initially high
 };
 
 #ifndef __cplusplus
 # include <stdbool.h>
 #endif
 
+/// Macro to indicate that a function parameter is unused
 #define UNUSED(_x)	(void)_x
+
+/// Assert macro, normally defined to do nothing
 #define Assert(expr) ((void) 0)
 
-// Macro to give us the number of elements in an array
 #ifndef ARRAY_SIZE
+/// Macro to give us the number of elements in an array
 # define ARRAY_SIZE(_x)	(sizeof(_x)/sizeof((_x)[0]))
 #endif
 
@@ -105,19 +111,65 @@ enum PinMode
 extern "C" {
 #endif
 
+/**
+ * @brief Get the time since startup
+ * @return Number of milliseconds since startup modulo 2^32
+ */
 uint32_t millis() noexcept;
+
+/**
+ * @brief Get the time since startup
+ * @return Number of milliseconds since startup modulo 2^64
+ */
 uint64_t millis64() noexcept;
+
+/**
+ * @brief Delay for a number of 1ms clock ticks.
+ * @param millis Number of 1ms system ticks to delay for. The actual delay may be up to 1 millisecond less than this, or arbitrarily greater.
+ */
 void delay(uint32_t millis) noexcept;
 
+/**
+ * @brief Set the required mode fore an I/O pin for
+ * @param pin The pin number to set the mode for. If the pin number is not valid (e.g. NoPin), the call will be ignored.
+ * @param mode The mode to set the pin to.
+ */
 void pinMode(Pin pin, enum PinMode mode) noexcept;
+
+/**
+ * @brief Read from an I/O pin
+ * @param pin The pin to read from
+ * @return true if the pin was valid and its state is high, false if it was not valid (e.g. NoPin) or it is valid and its state is low
+ */
 bool digitalRead(Pin pin) noexcept;
+
+/**
+ * @brief Write to an I/O pin
+ * @param pin The pin to write to. If the pin number is not valid (e.g. NoPin), the call will be ignored.
+ * @param high True to set the pin high, false to set it low
+ */
 void digitalWrite(Pin pin, bool high) noexcept;
 
+/**
+ * @brief Return a random or pseudo-random unsigned 32-bit integer
+ * @return The random number
+ */
 uint32_t random32(void) noexcept;		// needed by lwip
 
-// Delay for a specified number of CPU clock cycles from the starting time. Return the time at which we actually stopped waiting.
+/**
+ * @brief Delay for a specified number of CPU clock cycles from the starting time
+ *
+ * @param start The time we want to start waiting from, which must be in the recent past
+ * @param cycles The number of processor clock cycles to delay from the start time
+ * @return The time at which we actually stopped waiting.
+ */
 uint32_t DelayCycles(uint32_t start, uint32_t cycles) noexcept;
 
+/**
+ * @brief Delay or at least the specified number of microseconds
+ *
+ * @param How many microseconds to delay for
+ */
 static inline void delayMicroseconds(uint32_t) noexcept __attribute__((always_inline, unused));
 static inline void delayMicroseconds(uint32_t usec) noexcept
 {
@@ -125,25 +177,44 @@ static inline void delayMicroseconds(uint32_t usec) noexcept
 }
 
 // Functions and macros to enable/disable interrupts
+
+/**
+ * @brief Enable interrupts unconditionally
+ *
+ */
 static inline void cpu_irq_enable() noexcept
 {
 	__DMB();
 	__enable_irq();
 }
 
+/**
+ * @brief Disable interrupts unconditionally
+ *
+ */
 static inline void cpu_irq_disable() noexcept
 {
 	__disable_irq();
 	__DMB();
 }
 
-typedef bool irqflags_t;
+typedef bool irqflags_t;	///< Type used to indicate whether interrupts were enabled/should be enabled
 
+/**
+ * @brief Test whether interrupts are enabled
+ *
+ * @return True iff interrupts are enabled
+ */
 static inline bool cpu_irq_is_enabled() noexcept
 {
 	return __get_PRIMASK() == 0;
 }
 
+/**
+ * @brief Disable interrupts and return the initial enabled state. C++ clients should use class AtomicCriticalSectionLocker instead.
+ *
+ * @return The initial interrupts enabled state
+ */
 static inline irqflags_t cpu_irq_save() noexcept
 {
 	const irqflags_t flags = cpu_irq_is_enabled();
@@ -151,11 +222,22 @@ static inline irqflags_t cpu_irq_save() noexcept
 	return flags;
 }
 
+/**
+ * @brief convert irqflags_t to Boolean
+ *
+ * @param flags Value to convert
+ * @return Converted value
+ */
 static inline bool cpu_irq_is_enabled_flags(irqflags_t flags) noexcept
 {
 	return flags;
 }
 
+/**
+ * @brief Re-enable interrupts if they were enabled originally
+ *
+ * @param flags The original interrupts enabled state returned by a call to cpu_irq_save
+ */
 static inline void cpu_irq_restore(irqflags_t flags) noexcept
 {
 	if (cpu_irq_is_enabled_flags(flags))
@@ -164,12 +246,22 @@ static inline void cpu_irq_restore(irqflags_t flags) noexcept
 	}
 }
 
+/**
+ * @brief Return true of a character is one of the digits 0 thru 9
+ *
+ * @param c the character to test
+ * @return True iff the character is a digit
+ */
 static inline bool isDigit(char c) noexcept
 {
 	return isdigit(c) != 0;
 }
 
-// Return true if we are in any interrupt service routine
+/**
+ * @brief Test whether we are in an interrupt service routine or exception handler
+ *
+ * @return true if we are in an interrupt service routine or exception handler
+ */
 static inline bool inInterrupt() noexcept
 {
 	return (__get_IPSR() & 0x01FF) != 0;
