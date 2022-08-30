afa37a11faefe438e2118800aea5dc4bfd52565e
David Crocker <dcrocker@eschertech.com>
2022-08-30 10:53:42 +0100

    Set correct OS option for tinyusb

--- src/RP2040/tusb_config.h
+++ src/RP2040/tusb_config.h
@@ -9,7 +9,7 @@
 #define SRC_RP2040_TUSB_CONFIG_H_
 
 #define CFG_TUSB_MCU				OPT_MCU_RP2040
-#define CFG_TUSB_OS					OPT_OS_FREERTOS
+#define CFG_TUSB_OS					OPT_OS_PICO				// this is what the library for Arduino is compiled for
 #define CFG_TUSB_RHPORT0_MODE		(OPT_MODE_DEVICE | OPT_MODE_LOW_SPEED)
 #define CFG_TUD_CDC					1
 
