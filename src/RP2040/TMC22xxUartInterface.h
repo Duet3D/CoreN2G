75454b18464e3cdf20241d6b04527a5e2fe30a64
David Crocker <dcrocker@eschertech.com>
2022-09-18 21:31:43 +0100

    Added namespace TMC22xxUartInterface

---
+++ src/RP2040/TMC22xxUartInterface.h
@@ -0,0 +1,19 @@
+/*
+ * TMC22xxUartInterface.h
+ *
+ *  Created on: 18 Sept 2022
+ *      Author: David
+ *
+ * The functions in this namespace use a RP2040 PIO to drive the single-wire UART interface of one or more TMC2209 or TMC2240 drivers
+ */
+
+#ifndef SRC_RP2040_TMC22XXUARTINTERFACE_H_
+#define SRC_RP2040_TMC22XXUARTINTERFACE_H_
+
+#include <Core.h>
+
+namespace TMC22xxUartInterface
+{
+};
+
+#endif /* SRC_RP2040_TMC22XXUARTINTERFACE_H_ */
