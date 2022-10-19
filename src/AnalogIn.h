a68204f5b56595391485e37ef15b2f472cc22b72
David Crocker <dcrocker@eschertech.com>
2022-10-19 13:02:00 +0100

    Fix for SAM4E/4S/E70 builds

--- src/AnalogIn.h
+++ src/AnalogIn.h
@@ -73,10 +73,12 @@ namespace AnalogIn
 
 #else
 
+# if SAMC21 || SAME5x
 	// Simple analog input functions, for projects that don't use RTOS e.g. bootloaders
 	void Init(Adc * device);
 	void Disable(Adc * device);
 	uint16_t ReadChannel(Adc * device, uint8_t channel);
+# endif
 
 #endif
 }
