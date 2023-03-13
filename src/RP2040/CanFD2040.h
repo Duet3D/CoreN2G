e1324cb410766d9ee61609e1c5a5074a684953f1
David Crocker <dcrocker@eschertech.com>
2023-03-13 15:28:58 +0000

    Corrected comments

--- src/RP2040/CanFD2040.h
+++ src/RP2040/CanFD2040.h
@@ -5,7 +5,7 @@
  *      Author: David
  *
  * This is the header file for the low-level driver for partial ISO CAN-FD support on the RP2040.
- * It is derived from Kevin Connor's CAN 2.0 implementation for the RP2040, see https://github.com/KevinOConnor/can2040
+ * It is derived from Kevin O'Connor's CAN 2.0 implementation for the RP2040, see https://github.com/KevinOConnor/can2040
  * In particular, the PIO code is from that project and the low-level functions are derived from it.
  *
  * IMPORTANT! Robert Bosch GmbH owns many relevant patents and requires a license fee to be paid for any commercial use of CAN-FD.
