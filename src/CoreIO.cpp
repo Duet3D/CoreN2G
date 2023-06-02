5b25347615c394e8e07ef06410f3c89610b7619d
David Crocker <dcrocker@eschertech.com>
2023-06-02 14:07:04 +0100

    Timing fix for bit-banging Neopixels on RP2040

--- src/CoreIO.cpp
+++ src/CoreIO.cpp
@@ -35,7 +35,12 @@
 
 
 // Delay for a specified number of CPU clock cycles from the starting time. Return the time at which we actually stopped waiting.
-extern "C" uint32_t DelayCycles(uint32_t start, uint32_t cycles) noexcept
+extern "C"
+#if RP2040
+// When bit-banging Neopixels we can't afford to wait for instructions to be fetched from flash memory
+[[gnu::optimize("03")]] __attribute__((section(".time_critical")))
+#endif
+uint32_t DelayCycles(uint32_t start, uint32_t cycles) noexcept
 {
 	const uint32_t reload = (SysTick->LOAD & 0x00FFFFFF) + 1;
 	uint32_t now = start;
