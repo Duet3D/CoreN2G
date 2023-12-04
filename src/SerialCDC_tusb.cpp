d88907a734fbfebf7eb86f3b2deeb3d7ed73f590
David Crocker <dcrocker@eschertech.com>
2023-12-04 15:21:23 +0000

    Corrected exception specifications

--- src/SerialCDC_tusb.cpp
+++ src/SerialCDC_tusb.cpp
@@ -148,7 +148,7 @@ size_t SerialCDC::write(const uint8_t *buf, size_t length) noexcept
 
 // USB Device callbacks
 // Invoked when cdc when line state changed e.g connected/disconnected
-extern "C" void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) noexcept
+extern "C" void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts)
 {
 	(void) itf;
 	(void) rts;
@@ -156,7 +156,7 @@ extern "C" void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) noexcept
 }
 
 // Invoked when CDC interface received data from host
-extern "C" void tud_cdc_rx_cb(uint8_t itf) noexcept
+extern "C" void tud_cdc_rx_cb(uint8_t itf)
 {
 	(void) itf;
 }
