4726cb03a32ea7bee80d09d908c6be5a2d437136
dc42 <dcrocker@duet3d.com>
2026-03-17 18:54:30 +0000

    Moved SpiMode.h into this project and added SpiParameters.h

---
+++ src/SPI/SpiMode.h
@@ -0,0 +1,18 @@
+/*
+ * SpiMode.h
+ *
+ *  Created on: 16 Jun 2020
+ *      Author: David
+ */
+
+#ifndef SRC_HARDWARE_SPI_SPIMODE_H_
+#define SRC_HARDWARE_SPI_SPIMODE_H_
+
+#include <cstdint>
+
+enum class SpiMode : uint8_t
+{
+	mode0 = 0, mode1, mode2, mode3
+};
+
+#endif /* SRC_HARDWARE_SPI_SPIMODE_H_ */
---
+++ src/SPI/SpiParameters.h
@@ -0,0 +1,35 @@
+/*
+ * SpiParameters.h
+ *
+ *  Created on: 17 Mar 2026
+ *      Author: David
+ */
+
+#ifndef SRC_HARDWARE_SPI_SPIPARAMETERS_H_
+#define SRC_HARDWARE_SPI_SPIPARAMETERS_H_
+
+#include <CoreIO.h>
+#include <CoreTypes.h>
+
+// Structure to pass SPI device parameters. The details depend on the MCU.
+
+#if SAME5x
+
+struct SpiParameters
+{
+	uint8_t sercomNumber;
+	Pin mosiPin;
+	Pin misoPin;
+	Pin sclkPin;
+	GpioPinFunction pinFunction;
+	uint8_t dataInPad;
+	uint8_t dataOutPad;
+	DmaChannel dmaChanTx;
+	DmaChannel dmaChanRx;
+	DmaPriority dmaPrioTx;
+	DmaPriority dmaPrioRx;
+};
+
+#endif
+
+#endif /* SRC_HARDWARE_SPI_SPIPARAMETERS_H_ */
