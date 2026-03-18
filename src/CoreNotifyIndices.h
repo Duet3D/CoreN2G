00da9e4d4a6f907630b7ff6bff8d15ea1d6ccb5a
dc42 <dcrocker@duet3d.com>
2026-03-18 09:43:16 +0000

    Move SPI device stack in here from RRF project

--- src/CoreNotifyIndices.h
+++ src/CoreNotifyIndices.h
@@ -18,6 +18,7 @@ namespace NotifyIndices
 	constexpr uint32_t CanDevice = UartTx;					// CAN can share with UART
 	constexpr uint32_t Usb = UartTx;
 	constexpr uint32_t Sdhc = UartTx;
+	constexpr uint32_t Spi = UartTx;
 	constexpr uint32_t AnalogIn = UartTx + 1;				// analog in best have its own because it calls a hook function that may call device drivers
 	constexpr uint32_t NextAvailableAfterCore = NextAvailableAfterRTOS + 2;
 }
