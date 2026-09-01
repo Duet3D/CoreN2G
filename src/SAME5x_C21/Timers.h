8235858a85f5b4cf532046ae0b11fd19a53fcf37
dc42 <dcrocker@duet3d.com>
2026-09-01 12:45:41 +0100

    Further change to Timers.h to avoid possible issues

--- src/SAME5x_C21/Timers.h
+++ src/SAME5x_C21/Timers.h
@@ -17,9 +17,7 @@ namespace Timers
 		TC0, TC1, TC2, TC3, TC4,
 #if SAME5x
 		TC5,
-# if defined(__SAME54P20A__) || defined(__SAME54N20A__) || defined(__SAME51N19A__) || defined(__SAME51P20A__)
-		TC6, TC7					// CAUTION! lower pin count variants of SAME5x devices don't have these
-# endif
+		TC6, TC7					// CAUTION! lower pin count variants of SAME5x devices don't have these. We use the same build of CoreN2G for various MCUs.
 #endif
 	};
 
